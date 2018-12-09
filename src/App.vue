8cac0d6f2c64c41f0441f2dd6463ac47e7733ee8
Igor Guastalla de Lima <limaguastallaigor@gmail.com>
2018-12-09 12:19:16 -0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="container is-dark">
+      <h1 class="text-center">Vue-pixel-art</h1>
+    </section>
+    <div class="layout wrap-row mt-3 h100">
+      <section class="draw container with-title w50 h-container">
+        <h2 class="title">Draw</h2>
+      </section>
+      <section class="form container with-title w50 h-container">
+        <h2 class="title">Configuration</h2>
+      </section>
+    </div>
+    <github-corner/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GithubCorner from '@/components/GithubCorner.vue'
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
-  }
+    GithubCorner
+  },
+  data: () => ({
+
+  })
 }
 </script>
 
-<style lang="scss">
+<style>
+html, body {
+  height: calc(100% - 4em);
+  width: 100%;
+}
+</style>
+
+<style scoped lang="scss">
+@import './style/main.scss';
+
+$px: 2px;
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Press Start 2P', -apple-system, BlinkMacSystemFont, Segoe UI,
+    Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+    sans-serif;
+  height: 100%;
+  width: 100%;
+}
+
+.mt-3 {
+  margin-top: $px * 16;
+}
+
+.w50 {
+  width: 50%;
+}
+
+.h100 {
+  height: 100%;
+}
+
+.h-container {
+  height: calc(100% - 9em);
+}
+
+pre {
+  white-space: pre-wrap;
+  color: rgb(212, 0, 255);
+  line-height: 30px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'typeface-press-start-2p/index.css'
+import './style/nes.min.css'
 
 Vue.config.productionTip = false
 
